a42ff3fdb63e2bff27bd38fee6d9ff26081d8b91
wolfeeluo <249482315@qq.com>
2021-07-21 16:07:44 +0800

    add migration file

---
+++ server/migrations/06-00800-service-token-id-column.js
@@ -0,0 +1,26 @@
+const Sequelize = require('sequelize');
+
+/**
+ * @param {import('sequelize').QueryInterface} queryInterface
+ * @param {import('../lib/config')} config
+ * @param {import('../lib/logger')} appLog
+ * @param {object} sequelizeDb - sequelize instance
+ */
+// eslint-disable-next-line no-unused-vars
+async function up(queryInterface, config, appLog, sequelizeDb) {
+  try {
+    // alter id column from integer to string
+    await queryInterface.changeColumn('service_tokens', 'id', {
+      type: Sequelize.STRING,
+      primaryKey: true,
+      defaultValue: Sequelize.UUIDV4,
+    });
+  } catch (error) {
+    // ignore error. constraint may not exist depending on backend database
+    appLog.error(error, `Error alter id column from integer to string`);
+  }
+}
+
+module.exports = {
+  up,
+};
