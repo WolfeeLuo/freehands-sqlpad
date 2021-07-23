c9eff4cd39219ed5efa187d6e5c57339a1ffac67
wolfeeluo <249482315@qq.com>
2021-07-23 15:39:17 +0800

    handle postgresql error

--- server/migrations/06-00700-service-token-id-column.js
+++ server/migrations/06-00700-service-token-id-column.js
@@ -8,15 +8,23 @@ const Sequelize = require('sequelize');
  */
 // eslint-disable-next-line no-unused-vars
 async function up(queryInterface, config, appLog, sequelizeDb) {
-  try {
-    // alter id column from integer to string
-    await queryInterface.changeColumn('service_tokens', 'id', {
-      type: Sequelize.STRING,
-      defaultValue: Sequelize.UUIDV4,
-    });
-  } catch (error) {
-    // ignore error. constraint may not exist depending on backend database
-    appLog.error(error, `Error alter id column from integer to string`);
+  if (config.Sequelize.dialect === 'postgres') {
+    try{
+      const query = "ALTER TABLE \"service_tokens\" ALTER COLUMN \"id\" TYPE VARCHAR(255)"
+      await queryInterface.sequelize.query(query)
+    }catch(error){
+      appLog.error(error, `Error alter id column from integer to string under postgres`);
+    }
+  } else {
+    try {
+      // alter id column from integer to string
+      await queryInterface.changeColumn('service_tokens', 'id', {
+        type: Sequelize.STRING,
+        defaultValue: Sequelize.UUIDV4,
+      });
+    } catch (error) {
+      appLog.error(error, `Error alter id column from integer to string`);
+    }
   }
 }
 
